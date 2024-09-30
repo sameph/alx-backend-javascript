e4f34b9f8297e9c570d4b171ec08bd12205e4836
sameph <samuelephrem2012@gmail.com>
2024-10-01 01:04:39 +0300

    loops correct variable and to es6

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    const value = array[idx];
+  for (const value of array) {
+    const idx = array.indexOf(value);
     array[idx] = appendString + value; // eslint-disable-line no-param-reassign
   }
 
